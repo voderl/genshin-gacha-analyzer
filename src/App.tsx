f0408f1fbfbef669ecb8a7233bdf321a9bb6d281
voderl <voderl@qq.com>
2021-02-03 19:30:27 +0800

    fix: 移除router

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,15 @@
 /** @jsxImportSource @emotion/react */
-import { css } from '@emotion/react';
 import { useContext, useState } from 'react';
 import GlobalContext from './context/GlobalContext';
 import { LoadPage } from './pages/LoadPage';
 import { ShowPage } from './pages/ShowPage';
-import { Redirect, Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 
 import './App.css';
 import { WorkBook } from 'xlsx/types';
 
 function App() {
   const { workbook } = useContext(GlobalContext);
-  return (
-    <Router>
-      <Switch>
-        <Route path='/load'>
-          <LoadPage />
-        </Route>
-        <Route path='/show'>{workbook === null ? <Redirect to='/load' /> : <ShowPage />}</Route>
-        <Route path='/'>
-          <Redirect to='/load' />
-        </Route>
-      </Switch>
-    </Router>
-  );
+  return workbook === null ? <LoadPage /> : <ShowPage />;
 }
 function WrappedApp() {
   const [workbook, setWorkbook] = useState<WorkBook | null>(null);
