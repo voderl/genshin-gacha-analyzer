eeda9167833b30a77c5dc5580ed789c98aebf9b4
voderl <voderl@qq.com>
2021-03-20 16:27:53 +0800

    feat: update utils

---
+++ src/utils/renderPngTip.ts
@@ -0,0 +1,27 @@
+import { message } from 'antd';
+
+let id = 0;
+
+export default function renderPngTip(func: (resolve: () => void, reject: () => void) => void) {
+  const key = id++;
+  message.loading({
+    content: '生成图片中...',
+    key,
+  });
+  const loading = new Promise(func as any);
+  loading.then(
+    () => {
+      message.success({
+        content: '生成图片成功',
+        key,
+      });
+    },
+    (e: any) => {
+      console.log(e);
+      message.error({
+        content: '生成图片失败，请重试或更换浏览器',
+        key,
+      });
+    },
+  );
+}
