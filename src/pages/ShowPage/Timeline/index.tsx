20f31f61d64c93d2e2c2eca004b79dcf8ab0d068
voderl <voderl@qq.com>
2021-03-26 16:38:20 +0800

    feat: 使用堆叠条形图展示timeline

--- src/pages/ShowPage/Timeline/index.tsx
+++ src/pages/ShowPage/Timeline/index.tsx
@@ -7,24 +7,27 @@ import {
   ToolboxComponent,
   TooltipComponent,
   GridComponent,
+  LegendComponent,
   DataZoomComponent,
 } from 'echarts/components';
 import { BarChart } from 'echarts/charts';
 import { CanvasRenderer } from 'echarts/renderers';
 import { ECharts } from 'echarts/core';
-import { Data } from 'types';
+import { Data, DataItem } from 'types';
 import { COLOR, FONT_FAMILY, SHOW_DATA_ALL_KEY } from 'const';
 import { Alert, Switch } from 'antd';
 import memoize from 'lodash/memoize';
 import { getDateInfo } from './getDateInfo';
 import { CollapseWorkSheet } from './CollapseWorkSheet';
 import { useCacheMemo } from 'context/CacheContext';
+import get from 'lodash/get';
 
 echarts.use([
   TitleComponent,
   ToolboxComponent,
   TooltipComponent,
   GridComponent,
+  LegendComponent,
   DataZoomComponent,
   CanvasRenderer,
   BarChart,
@@ -32,40 +35,52 @@ echarts.use([
 type TimelineProps = {
   onGetData: (key: string) => Data;
 };
+type DateInfo = {
+  count: number;
+  '5': number;
+  '4': number;
+  '3': number;
+  day: string;
+};
 function calculateData(data: Data) {
-  const countByDay: Array<any> = [];
-  function addToList(current: { count: number; day: string; existFiveStar: boolean }) {
-    if (current.existFiveStar) {
-      countByDay.push({
-        value: [current.day, current.count],
-        itemStyle: {
-          color: COLOR.FIVE_STAR,
-        },
-      });
-    } else countByDay.push([current.day, current.count]);
+  const countByDay: Array<DateInfo> = [];
+  function addToList(current: DateInfo) {
+    countByDay.push(current);
   }
   const format = (index: number) => data[index].时间.slice(0, 10);
   const initCurrent = () => ({
     count: 0,
+    '5': 0,
+    '4': 0,
+    '3': 0,
     day: '',
-    existFiveStar: false,
   });
   let current = initCurrent();
+  const walk = (item: DataItem) => {
+    current.count += 1;
+    (current as any)[item.星级] += 1;
+  };
   for (let i = 0, len = data.length; i < len; i++) {
+    const item = data[i];
     const day = format(i);
     if (current.day === day) {
-      current.count += 1;
+      walk(item);
     } else {
       current.count && addToList(current);
       current = initCurrent();
-      current.count += 1;
       current.day = day;
+      walk(item);
     }
-    if (!current.existFiveStar && data[i].星级 === 5) current.existFiveStar = true;
   }
   current.count && addToList(current);
   return countByDay;
 }
+const barList = [
+  ['3 星', '3', COLOR.THREE_STAR],
+  ['4 星', '4', COLOR.FOUR_STAR],
+  ['5 星', '5', '#FAC858'],
+];
+
 export const Timeline: FC<TimelineProps> = function ({ onGetData }) {
   const echartsWrapper = useRef<HTMLDivElement>(null);
   const myChartRef = useRef<ECharts>();
@@ -73,23 +88,47 @@ export const Timeline: FC<TimelineProps> = function ({ onGetData }) {
   const [currentday, setCurrentDay] = useState<string>();
 
   // 获取数据，并写入缓存。
-  const getData = useCacheMemo(
+  const { getOption, getInfoByDay } = useCacheMemo(
     () => {
-      let getData: any;
-      getData = memoize((isHideZeroDay: boolean) => {
+      function formatToEchartsOption(dataList: DateInfo[]) {
+        return {
+          xAxis: {
+            type: 'category',
+            data: dataList.map(({ day }) => day),
+          },
+          series: barList.map(([name, key, color], index) => {
+            return {
+              name,
+              type: 'bar',
+              stack: 'total',
+              itemStyle: {
+                color,
+              },
+              data: dataList.map((data: any) => data[key]),
+            };
+          }),
+        };
+      }
+      function getData(isHideZeroDay: boolean) {
         if (!isHideZeroDay) {
-          const countByDay = getData(true);
+          const countByDay: DateInfo[] = getData(true);
           // 如果中间日期需要显示
-          const getDay = (data: any) => (Array.isArray(data) ? data[0] : data.value[0]);
+          const getDay = (data: DateInfo) => data.day;
           // 获取最开始一天和最后一天中间的日期
-          const dateList = getDateInfo(
+          const dateList: DateInfo[] = getDateInfo(
             getDay(countByDay[0]),
             getDay(countByDay[countByDay.length - 1]),
-          ).map((day: string) => [day, 0]);
+          ).map((day: string) => ({
+            count: 0,
+            '5': 0,
+            '4': 0,
+            '3': 0,
+            day,
+          }));
           let j = 0;
           for (let i = 0; i < countByDay.length; i++) {
             const day = getDay(countByDay[i]);
-            while (day !== dateList[j][0]) {
+            while (day !== dateList[j].day) {
               j++;
               if (j >= dateList.length) break;
             }
@@ -98,8 +137,21 @@ export const Timeline: FC<TimelineProps> = function ({ onGetData }) {
           return dateList;
         }
         return calculateData(onGetData(SHOW_DATA_ALL_KEY));
-      });
-      return getData;
+      }
+      const getOption = function (isHideZeroDay: boolean) {
+        return formatToEchartsOption(getData(isHideZeroDay));
+      };
+      let map: any;
+      function getInfoByDay(day: string): DateInfo | undefined {
+        if (map) return map[day];
+        map = Object.create(null);
+        getData(true).forEach((info) => (map[info.day] = info));
+        return getInfoByDay(day);
+      }
+      return {
+        getOption: memoize(getOption),
+        getInfoByDay,
+      };
     },
     [],
     'timeline',
@@ -116,13 +168,29 @@ export const Timeline: FC<TimelineProps> = function ({ onGetData }) {
         textStyle,
         tooltip: {
           trigger: 'axis',
-          position: function (pt: any) {
-            return [pt[0], '10%'];
+          formatter(series: any[]) {
+            if (series.length === 0) return '';
+            const day = series[0].axisValue;
+            const info = getInfoByDay(day);
+            const total = info ? info.count : 0;
+            return `<div style="font-size:14px;color:#666;font-weight:400;line-height:1.5;">
+              <div>${day}</div>
+              <div>当日抽卡次数: <span style="font-weight:900">${total}<span></div>
+              ${series
+                .reverse()
+                .map(
+                  (data) => `<div>
+                  ${data.marker}
+                  <span style="margin-left:2px">${data.seriesName}</span>
+                  <span style="float:right;margin-left:20px;font-weight:900;color=${data.color}">${data.value}</span>
+                </div>`,
+                )
+                .join('')}</div>`;
           },
         },
         title: {
           left: 'center',
-          text: '抽卡数据总览',
+          text: `抽卡数据总览(共${onGetData(SHOW_DATA_ALL_KEY).length}抽)`,
           textStyle,
         },
         toolbox: {
@@ -134,8 +202,12 @@ export const Timeline: FC<TimelineProps> = function ({ onGetData }) {
             saveAsImage: {},
           },
         },
+        legend: {
+          top: 25,
+          data: barList.map(([name]) => name),
+        },
         xAxis: {
-          type: 'category',
+          type: 'value',
           boundaryGap: false,
         },
         yAxis: {
@@ -153,43 +225,30 @@ export const Timeline: FC<TimelineProps> = function ({ onGetData }) {
             end: 100,
           },
         ],
-        series: [
-          {
-            name: '当日抽卡次数',
-            type: 'bar',
-            data: getData(true),
-          },
-        ],
       };
       options && myChart.setOption(options as any);
-      myChart.on('click', function (params: any) {
-        setCurrentDay(params.name);
+      myChart.getZr().on('click', function (params) {
+        const pointInPixel = [params.offsetX, params.offsetY];
+        if (myChart.containPixel('grid', pointInPixel)) {
+          let xIndex = myChart.convertFromPixel({ seriesIndex: 0 }, [
+            params.offsetX,
+            params.offsetY,
+          ])[0];
+          const option = myChart.getOption();
+          const day = get(option, `xAxis[0].data[${xIndex}]`) as string;
+          if (day) setCurrentDay(day);
+        }
       });
       myChartRef.current = myChart;
     }
-
     return () => {
       myChart && myChart.dispose();
     };
   }, []);
   useEffect(() => {
     if (myChartRef.current) {
-      myChartRef.current.setOption({
-        title: {
-          text: `抽卡数据总览(共${onGetData(SHOW_DATA_ALL_KEY).length}抽)`,
-        },
-        xAxis: {
-          type: 'category',
-          boundaryGap: false,
-        },
-        series: [
-          {
-            name: '当日抽卡次数',
-            type: 'bar',
-            data: getData(hideZeroDay),
-          },
-        ],
-      });
+      const option = getOption(hideZeroDay);
+      myChartRef.current.setOption(option);
     }
   }, [hideZeroDay]);
   const handleChangeSwitch = useCallback((checked: boolean) => {
