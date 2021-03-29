77ef6b704bbb02b73e1d9e937c2daee03770c8d3
voderl <voderl@qq.com>
2021-03-29 15:32:42 +0800

    fix: patch MiuiBrowser download by dataUrl failed

--- src/utils/downloadCanvas.ts
+++ src/utils/downloadCanvas.ts
@@ -3,11 +3,20 @@ import * as download from 'downloadjs';
 /**
  * 下载canvas为图片，手机部分浏览器不支持 createObjectUrl 等
  */
+const userAgent = window.navigator.userAgent;
+const IS_XIAOMI_BROWSER = userAgent.indexOf('MiuiBrowser') > -1;
 export default function downloadCanvas(
   canvas: HTMLCanvasElement,
   name: string,
   callback?: () => void,
 ) {
-  download(canvas.toDataURL(), name);
-  callback && callback();
+  if (IS_XIAOMI_BROWSER) {
+    canvas.toBlob((blob) => {
+      download(blob, name);
+      callback && callback();
+    });
+  } else {
+    download(canvas.toDataURL(), name);
+    callback && callback();
+  }
 }
