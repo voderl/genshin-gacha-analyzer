24df6a8b6f08fdda3ae67124cd767c9d2050647b
voderl <voderl@qq.com>
2021-09-08 00:37:00 +0800

    fix: 不信任excel文件中的"总次数""保底内"等数据，重新计算生成，同时重新排序 (#27)

--- src/utils/parseExcel.ts
+++ src/utils/parseExcel.ts
@@ -21,17 +21,12 @@ function makeFormatter(template: DataItem) {
       if (typeof item.星级 !== 'number') item.星级 = parseInt(item.星级);
     },
   ];
-  if (!('保底内' in template)) {
-    let count = 1;
-    formatters.push((item) => {
-      item.保底内 = count++;
-      if (item.星级 === 5) count = 1;
-    });
-  } else {
-    formatters.push((item) => {
-      if (typeof item.保底内 !== 'number') item.保底内 = parseInt(item.保底内);
-    });
-  }
+  // 抽卡记录合并可能会导致保底内数据重置，在此不信任"保底内"数据，重新生成
+  let count = 1;
+  formatters.push((item) => {
+    item.保底内 = count++;
+    if (item.星级 === 5) count = 1;
+  });
   return (item: DataItem, index: number) => {
     formatters.forEach((formatter) => formatter(item, index));
     return item;
@@ -46,23 +41,24 @@ export default function parseExcel(XLSX: typeof XLSXNameSpace, workbook: WorkBoo
     if (sheetName in POOL_NAME_TO_TYPE) {
       const type = (POOL_NAME_TO_TYPE as any)[sheetName];
       const sheet = sheets[sheetName];
-      const data = XLSX.utils.sheet_to_json(sheet) as DataItem[];
+      let data = XLSX.utils.sheet_to_json(sheet) as DataItem[];
+      // 只信任 抽卡数据里的 名称，日期，星级，其他数据按照时间顺序重新生成
       if (data.length !== 0) {
         const formatter = makeFormatter(data[0]);
-        if (!('总次数' in data[0])) {
-          sortBy(flatten(Object.values(data)), (item) => item.date).forEach((item, index) => {
-            item.总次数 = index + 1;
-          });
-        }
         data.forEach((info, index) => {
           info.pool = (POOL_TYPE_TO_NAME as any)[type];
           info.poolType = type;
           info.date = +parseToDate(info.时间);
-          formatter(info, index);
         });
+        data = sortBy(data, (item) => item.date);
+        data.forEach(formatter);
       }
       (result as any)[type] = data;
     } else throw new Error(`cannot parse sheetName ${sheetName}`);
+  });
+  // 不信任"总次数"数据，重新生成
+  sortBy(flatten(Object.values(result)), (item) => item.date).forEach((item, index) => {
+    item.总次数 = index + 1;
   });
   return result;
 }
