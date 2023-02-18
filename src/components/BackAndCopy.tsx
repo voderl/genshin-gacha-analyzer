d77414ea26e6cadfb44205e84e3c3da99589fb1b
voderl <voderl@qq.com>
2023-02-18 23:57:10 +0800

    feat: provide export button in chart page

--- src/components/BackAndCopy.tsx
+++ src/components/BackAndCopy.tsx
@@ -4,11 +4,12 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
 import { FC, useCallback, useState } from 'react';
-import { Button, message } from 'antd';
+import { Button, message, Modal, Tooltip } from 'antd';
 import RollbackOutlined from '@ant-design/icons/RollbackOutlined';
 import CopyOutlined from '@ant-design/icons/CopyOutlined';
 import CheckOutlined from '@ant-design/icons/CheckOutlined';
 import CloseOutlined from '@ant-design/icons/CloseOutlined';
+import DownloadOutlined from '@ant-design/icons/DownloadOutlined';
 import { useGlobalContext } from 'context/GlobalContext';
 import copy from 'copy-to-clipboard';
 
@@ -56,8 +57,14 @@ const copyIconMap = {
   [ECopyStatus.Fail]: <CloseOutlined />,
 };
 
+function collectEvent(name: string, data?: any) {
+  if ((window as any).gtag) {
+    (window as any).gtag('event', name, data);
+  }
+}
+
 export const BackAndCopy: FC<BackAndCopyProps> = function () {
-  const { isVertical, updatePage } = useGlobalContext();
+  const { isVertical, updatePage, parsedData } = useGlobalContext();
   const [copyStatus, updateCopyStatus] = useState(ECopyStatus.Pending);
   const handleBackToLoadPage = useCallback(() => {
     updatePage('');
@@ -71,6 +78,44 @@ export const BackAndCopy: FC<BackAndCopyProps> = function () {
       updateCopyStatus(ECopyStatus.Fail);
     }
   }, []);
+
+  const handleExport = useCallback(() => {
+    Modal.confirm({
+      title: '是否导出为 Excel 文件？',
+      content: [
+        <p>
+          在导入源文件仍然存在的情况请直接使用源文件。导出的 Excel
+          文件由抽卡记录数据重新生成，可能和导入源文件不同(比如不包含额外信息、角色活动祈愿-2
+          合并到角色活动祈愿)。
+        </p>,
+        <p>部分手机浏览器可能不支持导出。</p>,
+      ],
+      onOk() {
+        return import('pages/MergePage/MergeShow/downloadExcel')
+          .then((module) => {
+            const downloadExcel = module.default;
+            return downloadExcel(parsedData).then(() => {
+              message.success('导出成功');
+              collectEvent('export_excel');
+            });
+          })
+          .catch((err) => {
+            const getErrorMessage = (e: any) => {
+              if (typeof e === 'string') return e;
+              if (typeof e === 'object' && 'message' in e) return e.message;
+              return '未知错误';
+            };
+            const errorMessage = getErrorMessage(err);
+            message.error(`导出失败, ${errorMessage}`);
+            collectEvent('export_excel_fail', {
+              message: errorMessage,
+            });
+            console.log(err);
+          });
+      },
+    });
+  }, [parsedData]);
+
   if (!window.location.hash) return <></>;
   return (
     <div
@@ -90,6 +135,9 @@ export const BackAndCopy: FC<BackAndCopyProps> = function () {
       >
         {copyTextMap[copyStatus]}
       </Button>
+      <Tooltip title='导出为 Excel 文件'>
+        <Button icon={<DownloadOutlined />} onClick={handleExport} />
+      </Tooltip>
     </div>
   );
 };
